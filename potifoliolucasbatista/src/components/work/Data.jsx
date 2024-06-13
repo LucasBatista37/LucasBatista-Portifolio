f4ade0ce2a6f2dd8056b3986f38c9a128b6ed174
Lucas Batista <lucas.batista9734@gmail.com>
2024-06-12 22:22:32 -0300

    first commit

---
+++ potifoliolucasbatista/src/components/work/Data.jsx
@@ -0,0 +1,38 @@
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
+
+
+export const projectsData = [
+    {
+        id: 1,
+        Image: Work1,
+        title: 'Swift Post',
+        category: 'web',
+        link: 'https://swift-post-portifolio-qh1m.vercel.app/'
+    },
+    {
+        id: 2,
+        Image: Work2,
+        title: 'All Movies',
+        category: 'web',
+        link: 'https://all-movie-films.vercel.app/'
+    },
+];
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'web',
+    },
+    {
+        name: 'app',
+    },
+    {
+        name: 'design',
+    },
+]
