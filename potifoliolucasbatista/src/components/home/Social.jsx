1213b16aaff4188380761a3952f8c71711b4f1f5
Lucas Batista <lucas.batista9734@gmail.com>
2024-06-14 22:21:30 -0300

    mudando

--- potifoliolucasbatista/src/components/home/Social.jsx
+++ potifoliolucasbatista/src/components/home/Social.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Social = () => {
   return (
     <div className="home__social">
         <a href="https://www.instagram.com/lucas_batista38?igsh=anQ5ZmY4emRleXox" className="home__social-icon" target='_blank'>
-        <i class="uil uil-instagram"></i>
+        <i className="uil uil-instagram"></i>
         </a>
 
         <a href="https://www.linkedin.com/in/lucas-batista-004212263/" className="home__social-icon" target='_blank'>
-            <i class="uil uil-linkedin"></i>
+            <i className="uil uil-linkedin"></i>
         </a>
 
         <a href="https://github.com/LucasBatista37" className="home__social-icon" target='_blank'>
-            <i class="uil uil-github-alt"></i>
+            <i className="uil uil-github-alt"></i>
         </a>
-
     </div>
-  )
-}
+  );
+};
 
-export default Social
+export default Social;
