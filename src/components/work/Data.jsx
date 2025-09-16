ccb14ae09fdb55295acd169f4c6058902b527d97
Lucas Batista <lucas.batista9734@gmail.com>
2025-09-16 00:40:13 -0300

    Implement code changes to enhance functionality and improve performance

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -3,6 +3,7 @@ import CondoView from "../../assets/condoview.png";
 import Beyond from "../../assets/beyond.png";
 import PetLitoral from "../../assets/petlitoral.png";
 import ReactGram from "../../assets/reactgram.png";
+import PetCare from "../../assets/PetCare.png";
 import Work1 from "../../assets/swift.jpg";
 import Work2 from "../../assets/movie.jpg";
 import Work3 from "../../assets/itask.jpg";
@@ -32,7 +33,7 @@ export const projectsData = [
         id: 3,
         Image: PetLitoral,
         title: 'Pet Litoral',
-        link: 'www.petlitoral.shop',
+        link: 'https://petlitoral.shop',
         github: 'https://github.com/LucasBatista37/Pet-Litoral-Page',
         description: 'Site feito para agendamento de banho e tosa feito para o pet shop Pet Litoral.',
         technologies: ['React'],
@@ -50,6 +51,16 @@ export const projectsData = [
     }, 
     {
         id: 5,
+        Image: PetCare,
+        title: 'PetCare',
+        link: 'www.petcare.shop',
+        github: 'https://github.com/LucasBatista37/Beyond-Site',
+        description: 'Sistema de Gestão para Pet Shops.',
+        technologies: ['React', 'Tawind', 'NodeJs', 'MongoDB'],
+        category: 'web'
+    },
+    {
+        id: 6,
         Image: Work1,
         title: 'MiniBlog',
         link: 'https://github.com/LucasBatista37/Miniblog/wiki/Prints-do-Projeto',
@@ -59,7 +70,7 @@ export const projectsData = [
         category: 'web'
     }, 
     {
-        id: 6,
+        id: 7,
         Image: ReactGram,
         title: 'ReactGram',
         link: 'https://github.com/LucasBatista37/ReactGram/wiki/Prints-do-Projeto',
@@ -69,7 +80,7 @@ export const projectsData = [
         category: 'web'
     },
     {
-        id: 7,
+        id: 8,
         Image: Work2,
         title: 'Central Filmes',
         link: 'https://centralfilmes.vercel.app/',
@@ -79,12 +90,12 @@ export const projectsData = [
         category: 'web'
     },
     {
-        id: 8,
+        id: 9,
         Image: Work3,
         title: 'Tarefa Rápida',
         link: 'https://itask-typescript.vercel.app/',
         github: 'https://github.com/LucasBatista37/Task_Typescript',
-        description: 'Aplicativo para gerenciamento de tarefas.',
+        description: 'Site para gerenciamento de tarefas.',
         technologies: ['TypeScript', 'React'],
         category: 'web'
     },
